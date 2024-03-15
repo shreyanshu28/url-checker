df2295294631114ec25cf951807f0230208d7b13
shreyanshu28 <shreyanshuvyas@gmail.com>
2024-03-15 13:59:30 +0100

    added throttling and server mocking with type checking

--- checkURL.js
+++ checkURL.js
@@ -1,6 +1,64 @@
 
-const url = document.getElementById("url-validator")
+const urlInput = document.getElementById("url-checker")
+const urlMsg = document.getElementById("checker-output")
 
-url.addEventListener('keyup', function(e) {
-    console.log(e.target.value)
-})
+let searchVal = ''
+
+const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+
+const regex = new RegExp(expression);
+
+function throttle(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+function validateUrlFormat(url) {
+    return regex.test(url)
+}
+
+function isFile(url) {
+    return URL.split('/').pop().indexOf('.') > -1
+}
+
+function isFolder(url) {
+    return URL.split('/').pop().indexOf('.') === -1
+}
+
+
+function checkUrlExistence(url) {
+    if (validateUrlFormat(url)) {
+        setTimeout(() => {
+            const exists = Math.random() < 0.5;
+
+            if (exists) {
+                document.getElementById("checker-output").innerHTML = "The URL exists";
+
+                if (isFile(url)) {
+                    document.getElementById("checker-output").innerHTML = "The URL exists and is a file";
+                }
+
+                else if (isFolder(url)) {
+                    document.getElementById("checker-output").innerHTML = "The URL exists and is a folder";
+                }
+
+            } else {
+                document.getElementById("checker-output").innerHTML = "URL does not exist";
+            }
+        }, 1000);
+    } else {
+        document.getElementById("checker-output").innerHTML = "Invalid URL format";
+    }
+}
+
+
+urlInput.addEventListener('keyup', throttle((e) => {
+    const searchVal = e.target.value.trim();
+    console.log("searchVal", searchVal)
+    checkUrlExistence(searchVal);
+}, 500));
